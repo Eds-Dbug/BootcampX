f34f198b19378dc10a00b809a30e28c4838de68a
Eds-Dbug <edsdbug@gmail.com>
2022-08-24 23:54:15 +0000

    student and teacher to use paramatize queries.

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,30 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+const cohortName = process.argv[2];
+
+const query = {
+  text: 'INSERT INTO users(name, email) VALUES($1, $2)',
+  values: [cohortName],
+}
+
+pool.query(`
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = $1
+ORDER BY teacher;
+`, [`${cohortName}` || 'JUL02'])
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.cohort}: ${user.teacher}`);
+  })
+}).catch(err => console.error('query error', err.stack));
